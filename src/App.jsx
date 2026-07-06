6677bb6dfc6bc55aa64a615762e35f356bf479bd
Nandhakumar2550 <nm8573@srmist.edu.in>
2026-07-06 11:01:42 +0530

    [Nandhakumar] FUC2 - Routing & Authentication UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import AppRoutes from "./routes/AppRoutes";
+
 function App() {
-  return (
-    <h1>Fundoo Notes Frontend</h1>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,54 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
+import NotFound from "../pages/NotFound";
+
+import MainLayout from "../layouts/MainLayout";
+import ProtectedRoute from "../components/ProtectedRoute";
+
+function AppRoutes() {
+
+  return (
+
+    <BrowserRouter>
+
+      <MainLayout>
+
+        <Routes>
+
+          <Route
+            path="/"
+            element={<Login />}
+          />
+
+          <Route
+            path="/register"
+            element={<Register />}
+          />
+
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="*"
+            element={<NotFound />}
+          />
+
+        </Routes>
+
+      </MainLayout>
+
+    </BrowserRouter>
+
+  );
+}
+
+export default AppRoutes;
